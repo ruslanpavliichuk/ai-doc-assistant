b1b0efe0dab854aa362952b870d5313fc16b6e77
ruslanpavliichuk <pavliichukruslan@gmail.com>
2025-08-24 23:33:46 +0100

    Added functions for parsing data

--- src/data_processing/parser.py
+++ src/data_processing/parser.py
@@ -1,1 +1,39 @@
 # Module for parsing HTML and PDF
+
+from bs4 import BeautifulSoup
+import pymupdf
+
+
+def parse_html(html_content):
+    """
+    Parse HTML content and extract text.
+
+    Args:
+        html_content (str): The HTML content to parse.
+
+    Returns:
+        str: Extracted text from the HTML.
+    """
+    soup = BeautifulSoup(html_content, 'html.parser')
+    return soup.get_text(separator='\n', strip=True)
+
+def parse_pdf(file_path):
+    """
+    Parse PDF file and extract text.
+
+    Args:
+        file_path (str): The path to the PDF file.
+
+    Returns:
+        str: Extracted text from the PDF.
+    """
+
+    all_text = []
+    try:
+        with pymupdf.open(file_path) as doc:
+            for page in doc:
+                all_text.append(page.get_text(sort=True))
+        return "\f".join(all_text)
+    except Exception as e:
+        print(f"Error processing file {file_path}: {e}")
+        return ""
