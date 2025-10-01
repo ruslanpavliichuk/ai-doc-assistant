70489dfa682626f3116ad618a26e9e0913b214dc
ruslanpavliichuk <pavliichukruslan@gmail.com>
2025-10-01 13:41:55 +0100

    Updated imports

--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,3 +1,5 @@
-from . import parser
-from . import chunker
+from .parser import parse_pdf, parse_html
+from .chunker import TokenChunker
+from .models import Chunk
 
+__all__ = ["parse_pdf", "parse_html", "TokenChunker", "Chunk"]
